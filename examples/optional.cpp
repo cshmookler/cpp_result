5a91aeb03d9d7447b5623a285c7042ba0fc42427
Caden Shmookler <cshmookler@gmail.com>
2025-01-18 05:09:33 -0700

    Added examples for result_t and optional_t.

---
+++ examples/optional.cpp
@@ -0,0 +1,61 @@
+// Standard includes
+#include <cstddef>
+#include <iostream>
+#include <vector>
+
+// External includes
+#include <cpp_result/optional.hpp>
+
+res::optional_t<size_t> multiply(size_t lhs, size_t rhs) {
+    if (lhs == rhs) {
+        return RES_NEW_ERROR("lhs and rhs cannot be the same value");
+    }
+
+    return lhs * rhs;
+}
+
+res::optional_t<std::vector<size_t>> func(size_t count) {
+    if (count == 0) {
+        return RES_NEW_ERROR("count cannot be zero");
+    }
+
+    const size_t limit = 100000;
+
+    std::vector<size_t> values;
+
+    for (size_t _ = 0; _ < count; ++_) {
+        auto product = multiply(limit - count, count);
+        if (product.has_error()) {
+            return RES_TRACE(product.error());
+        }
+
+        values.push_back(product.value());
+    }
+
+    return values;
+}
+
+int main() {
+    // This operation succeeds.
+    auto res = func(5);
+    if (res.has_error()) {
+        std::cout << res.error() << '\n';
+        // return 1; // NOTE: You'd normally return here.
+    }
+
+    // This operation fails.
+    res = func(0);
+    if (res.has_error()) {
+        std::cout << res.error() << '\n';
+        // return 1; // NOTE: You'd normally return here.
+    }
+
+    // This operation also fails.
+    res = func(50000);
+    if (res.has_error()) {
+        std::cout << res.error() << '\n';
+        // return 1; // NOTE: You'd normally return here.
+    }
+
+    return 0;
+}
---
+++ examples/result.cpp
@@ -0,0 +1,31 @@
+// Standard includes
+#include <iostream>
+
+// External includes
+#include <cpp_result/result.hpp>
+
+res::result_t func(bool value) {
+    if (value) {
+        return res::success;
+    }
+
+    return RES_NEW_ERROR("Operation failed!");
+}
+
+int main() {
+    // This operation succeeds.
+    auto res = func(true);
+    if (res.failure()) {
+        std::cout << res.error() << '\n';
+        // return 1; // NOTE: You'd normally return here.
+    }
+
+    // This operation fails.
+    res = func(false);
+    if (res.failure()) {
+        std::cout << res.error() << '\n';
+        // return 1; // NOTE: You'd normally return here.
+    }
+
+    return 0;
+}
